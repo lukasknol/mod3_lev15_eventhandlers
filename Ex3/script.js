e6210b939e34dde56bb07e221191a7ea58e349df
Lukas <79308537+lukasknol@users.noreply.github.com>
2021-04-06 11:17:33 +0200

    Add files via upload
    Module 3, Level 15: Event handlers (defer)

---
+++ Ex3/script.js
@@ -0,0 +1,21 @@
+const attachEventToAlertButton = function () {
+  const alertButton = document.getElementById("alert-button");
+  alertButton.addEventListener("click", function () {
+    alert("Hello World!");
+  });
+};
+
+const toggleColor = function () {
+  const bodyElement = document.body;
+  bodyElement.classList.toggle("red-background");
+};
+
+const attachEventToChangeColorButton = function () {
+  const changeColorButton = document.getElementById("change-background-button");
+  changeColorButton.addEventListener("click", function () {
+    toggleColor();
+  });
+};
+
+attachEventToAlertButton();
+attachEventToChangeColorButton();
